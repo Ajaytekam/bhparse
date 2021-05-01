f27a09a6a6759edbfcd2dde81435be1263e530a8
Ajay Tekam <x90ajay@gmail.com>
2021-05-01 15:16:24 +0530

    installer code updated

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
 END='\033[0m'
 
-echo "${YELLOW}[+]${GREEN} bhparse installer...${END}" 
+echo -e "${YELLOW}[+]${GREEN} bhparse installer...${END}" 
 mkdir -p ~/tools 
 cp bhparse.py ~/tools  
-pushd ~/tools 
+cd ~/tools 
 chmod +x bhparse.py 
 # addiing aliases into bashrc file 
 echo "alias bhparse='~/tools/bhparse.py'" >> ~/.bashrc   
 echo "complete -W \"\\\$(ls | egrep '*.xml' --color=never)\" bhparse" >> ~/.bashrc  
-echo "${YELLOW}[+]${GREEN} Installation complete${END}" 
+echo -e "\n${YELLOW}[*]${GREEN} Installation Completed!${END}"
+echo -e "${YELLOW}[!]${GREEN} Run Command ${CYAN}'source ~/.bashrc'${END}"
